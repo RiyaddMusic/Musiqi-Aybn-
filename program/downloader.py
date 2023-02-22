e6343f9856fc35d1f5a512fae843310faac38114
RiyaddMusic <106576944+RiyaddMusic@users.noreply.github.com>
2023-02-22 11:40:09 +0400

    Update downloader.py

--- program/downloader.py
+++ program/downloader.py
@@ -35,7 +35,7 @@ ydl_opts = {
 }
 
 
-@Client.on_message(command(["bul", f"song@{bn}"]) & ~filters.edited)
+@Client.on_message(command(["song", f"song@{bn}"]) & ~filters.edited)
 def song(_, message):
     query = " ".join(message.command[1:])
     m = message.reply("🔎 Musiqi axtarılır...")
